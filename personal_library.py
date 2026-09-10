534d6a7ca637375a795e42a71eca848b39e2ec5a
JacobM215 <jlmorris2000@icloud.com>
2026-09-10 14:53:21 -0400

    (feat) Add display menu for personal library.

--- personal_library.py
+++ personal_library.py
@@ -1,6 +1,27 @@
-def main():
-    print("Testing Entry Point")
+"""
+Displays the menu for the personal library application.
+This menu is invoked by the main function in the personal_library.py file.
 
+Parameters:
+    None
+Returns:
+    None
+"""
+
+
+def display_menu():
+    print("Please select an option:")
+    print("------------------------")
+    print("1. Add Book Title")
+    print("2. Remove Book Title")
+    print("3. List Book Titles")
+    print("4. Search Book Title")
+    print("5. Exit")
+    print("")
+
+
+def main():
+    display_menu()
 
 if __name__ == "__main__":
     main()
