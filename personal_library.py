57fd6b0be934fa6d845f762d2357c76d14169453
JacobM215 <jlmorris2000@icloud.com>
2026-09-17 14:28:21 -0400

    (feat) add book title for personal library

--- personal_library.py
+++ personal_library.py
@@ -5,23 +5,56 @@ This menu is invoked by the main function in the personal_library.py file.
 Parameters:
     None
 Returns:
-    None
+    output
 """
 
-
 def display_menu():
-    print("Please select an option:")
+    print("Please select an option (1-5):")
     print("------------------------")
     print("1. Add Book Title")
     print("2. Remove Book Title")
     print("3. List Book Titles")
     print("4. Search Book Title")
     print("5. Exit")
-    print("")
+    print()
 
+    option = input("Enter your choice: ")
+    return option
+
+"""
+Allows user to enter book title, author, and year it was published.
+The option is envoked when the user's input = 1.
+
+Parameters:
+    library
+Returns:
+    None
+"""
+
+def add_book_title(library):
+    
+    book_title = input("Enter the book title to add: ")
+    book_author = input("Enter the author: ")
+    book_year = input("Enter the year of the book: ")
+
+    library[book_title] = {
+        "book_author": book_author,
+        "book_year": book_year 
+    }
+
+    print(f"Book title '{book_title}' has been added to your personal library.")
 
 def main():
-    display_menu()
+    library = {}
+    keep_running = True
+
+    while keep_running:
+        option = display_menu()
+
+        if option == "1":
+            add_book_title(library)
+        else:
+            print("Invalid Choice. Please enter (1, 2, 3, 4, or 5)")
 
 if __name__ == "__main__":
     main()
---
+++ test_personal_library.py
@@ -0,0 +1,3 @@
+# virtual enviroment
+from person_library import display_menu, add_book_title, main
+
